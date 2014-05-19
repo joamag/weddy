b5f061c6a7c43fa1fe3f3418241b4b3e804af941
joamag <joamag@gmail.com>
2014-05-19 20:33:43 +0100

    better code structure

--- src/weddy/controllers/set.py
+++ src/weddy/controllers/set.py
@@ -8,7 +8,6 @@ class SetController(appier.Controller):
     @appier.route("/sets", "GET")
     @appier.private
     def list(self):
-
         return self.template(
             "index.html.tpl"
         )
--- src/weddy/models/instance.py
+++ src/weddy/models/instance.py
@@ -6,19 +6,33 @@ import appier
 import appier_extras
 
 class Instance(appier_extras.admin.Account):
-    
-    oauth_token = appier.field()
-    
-    oauth_token_secret = appier.field()
-    
+
+    client_key = appier.field(
+        index = True
+    )
+
+    client_secret = appier.field(
+        private = True
+    )
+
+    oauth_token = appier.field(
+        private = True
+    )
+
+    oauth_token_secret = appier.field(
+        private = True
+    )
+
     @classmethod
-    def login(cls, username, password): 
-        account = super(Instance, cls).login(username, password)        
+    def login(cls, username, password):
+        account = super(Instance, cls).login(username, password)
         return account
 
-
-        #self.oauth_token = kwargs.get("oauth_token", None)
-        #self.oauth_token_secret = kwargs.get("oauth_token_secret", None)
-
     def get_api(self):
-        return flickr.Api(oauth_token = oauth_token)
+        instance = self.reload(rules = False)
+        return flickr.Api(
+            client_key = instance.client_key,
+            client_secret = instance.client_secret,
+            oauth_token = instance.oauth_token,
+            oauth_token_secret = instance.oauth_token_secret
+        )
